deb98c7ed518a611e076c3506e4bd49b18337824
jeffreytse <jeffreytse.mail@gmail.com>
2020-05-21 15:17:53 +0800

    refactor: perfect emoji processor

--- lib/jekyll-spaceship/processors/emoji-processor.rb
+++ lib/jekyll-spaceship/processors/emoji-processor.rb
@@ -1,65 +1,38 @@
 # frozen_string_literal: true
 
 require 'net/http'
-require 'uri'
 require 'json'
+require 'gemoji'
 
 module Jekyll::Spaceship
   class EmojiProcessor < Processor
-    EMOJI_MARKUP_HOST = 'https://api.github.com/emojis'
-    EMOJI_MARKUP_DATA = {}
-
-    def initialize
-      super()
-      self.initialize_emoji_data
-    end
-
-    def initialize_emoji_data
-      EMOJI_MARKUP_DATA.update get_emoji_markup_data
-    end
+    EMOJI_MARKUP_HOST = 'https://github.githubassets.com/images/icons/emoji/'
 
     def on_handle_html(content)
-      return content if EMOJI_MARKUP_DATA.size.zero?
       # handle emoji markup
-      content.scan(/:([\w+-]+):/) do |match_data|
-        emoji_markup = match_data[0]
-        emoji_image = EMOJI_MARKUP_DATA[emoji_markup]
-        next if emoji_image.nil?
+      content.scan(/:([\w\d+-]+):/) do |match|
+        emoji = Emoji.find_by_alias match[0]
+        next if emoji.nil?
         self.handled = true
 
-        # convert hex string to unicode
-        unicode = emoji_image.match(/unicode\/([\d\w]+)/)
-        if unicode[1]
-          unicode = "0x#{unicode[1]}".to_i(16)
-          alt = [unicode].pack('U*')
-        end
-        alt = emoji_markup if alt.nil?
+        # escape plus sign
+        emoji_name = emoji.name.gsub('+', '\\\+')
 
         content = content.gsub(
-          ":#{emoji_markup}:",
-          "<image class=\"emoji\" \
-            title=\"#{emoji_markup}\" \
-            alt=\"#{alt}\" \
-            src=\"#{emoji_image}\" \
+          /(?<!\=")\s*:#{emoji_name}:\s*(?!"\s)/,
+          "<img class=\"emoji\" \
+            title=\":#{emoji.name}:\" \
+            alt=\":#{emoji.name}:\" \
+            raw=\"#{emoji.raw}\" \
+            src=\"#{EMOJI_MARKUP_HOST}#{emoji.image_filename}\" \
             style=\"vertical-align: middle; \
             max-width: 1em; visibility: hidden;\" \
             onload=\"this.style.visibility='visible'\" \
-            onerror=\"this.replaceWith(this.alt)\"> \
-          </image>"
+            onerror=\"this.replaceWith(this.getAttribute('raw'))\"> \
+          </img>"
         )
       end
       content
-    end
-
-    def get_emoji_markup_data
-      data = {}
-      begin
-        source = Net::HTTP.get URI(EMOJI_MARKUP_HOST)
-        data = JSON.parse(source)
-      rescue StandardError => msg
-        logger.log msg
-      end
-      data
     end
   end
 end
