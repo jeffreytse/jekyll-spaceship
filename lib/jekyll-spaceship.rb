68f69a9ffe6fa9a14ed00b40bfe3c0de02c644d4
jeffreytse <jeffreytse.mail@gmail.com>
2020-05-26 13:38:32 +0800

    feat: use processors by config

--- lib/jekyll-spaceship.rb
+++ lib/jekyll-spaceship.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 require 'jekyll-spaceship/cores/logger'
+require 'jekyll-spaceship/cores/config'
 require 'jekyll-spaceship/cores/manager'
 require 'jekyll-spaceship/cores/processor'
 require 'jekyll-spaceship/cores/register'
 
 module Jekyll::Spaceship
   Logger.display_info
-  Register.use 'table-processor'
-  Register.use 'mathjax-processor'
-  Register.use 'plantuml-processor'
-  Register.use 'polyfill-processor'
-  Register.use 'video-processor'
-  Register.use 'emoji-processor'
+  Config.load
 end
