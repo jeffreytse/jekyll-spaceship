4329d9525be57845197509e04e0b8873800bbd1f
jeffreytse <jeffreytse.mail@gmail.com>
2020-10-12 18:45:12 +0800

    fix: relative url not working for media link (#28)

--- lib/jekyll-spaceship/processors/media-processor.rb
+++ lib/jekyll-spaceship/processors/media-processor.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'uri'
+require "nokogiri"
 
 module Jekyll::Spaceship
   class MediaProcessor < Processor
@@ -18,23 +19,29 @@ module Jekyll::Spaceship
       }
     end
 
-    def on_handle_markdown(content)
-      content = handle_normal_audio(content)
-      content = handle_normal_video(content)
-      content = handle_youtube(content)
-      content = handle_vimeo(content)
-      content = handle_dailymotion(content)
-      content = handle_spotify(content)
-      content = handle_soundcloud(content)
+    def on_handle_html(content)
+      # use nokogiri to parse html content
+      doc = Nokogiri::HTML(content)
+      # handle each img tag
+      doc.css('img').each do |element|
+        handle_normal_audio(element)
+        handle_normal_video(element)
+        handle_youtube(element)
+        handle_vimeo(element)
+        handle_dailymotion(element)
+        handle_spotify(element)
+        handle_soundcloud(element)
+      end
+      doc.to_html
     end
 
     # Examples:
     # ![audio](//www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3)
     # ![audio](//www.expample.com/examples/t-rex-roar.mp3?autoplay=true&loop=true)
-    def handle_normal_audio(content)
-      handle_media(content, {
+    def handle_normal_audio(element)
+      handle_media(element, {
         media_type: 'audio',
-        host: '(https?:)?\\/\\/.*\\/',
+        host: '(https?:\\/\\/)?.*\\/',
         id: '(.+?\\.(mp3|wav|ogg|mid|midi|aac|wma))',
       })
     end
@@ -44,10 +51,10 @@ module Jekyll::Spaceship
     # ![video](//www.html5rocks.com/en/tutorials/video/basics/devstories.webm)
     # ![video](//techslides.com/demos/sample-videos/small.ogv?allow=autoplay)
     # ![video](//techslides.com/demos/sample-videos/small.mp4?width=400)
-    def handle_normal_video(content)
-      handle_media(content, {
+    def handle_normal_video(element)
+      handle_media(element, {
         media_type: 'iframe',
-        host: '(https?:)?\\/\\/.*\\/',
+        host: '(https?:\\/\\/)?.*\\/',
         id: '(.+?\\.(avi|mp4|webm|ogg|ogv|flv|mkv|mov|wmv|3gp|rmvb|asf))'
       })
     end
@@ -56,8 +63,8 @@ module Jekyll::Spaceship
     # ![youtube](https://www.youtube.com/watch?v=XA2WjJbmmoM "title")
     # ![youtube](http://www.youtube.com/embed/w-m_yZCLF5Q)
     # ![youtube](//youtu.be/mEP3YXaSww8?height=100%&width=400)
-    def handle_youtube(content)
-      handle_media(content, {
+    def handle_youtube(element)
+      handle_media(element, {
         media_type: 'iframe',
         host: '(https?:)?\\/\\/.*youtu.*',
         id: '(?<=\\?v\\=|embed\\/|\\.be\\/)([a-zA-Z0-9\\_\\-]+)',
@@ -68,8 +75,8 @@ module Jekyll::Spaceship
     # Examples:
     # ![vimeo](https://vimeo.com/263856289)
     # ![vimeo](https://vimeo.com/263856289?height=100%&width=400)
-    def handle_vimeo(content)
-      handle_media(content, {
+    def handle_vimeo(element)
+      handle_media(element, {
         media_type: 'iframe',
         host: '(https?:)?\\/\\/vimeo\\.com\\/',
         id: '([0-9]+)',
@@ -80,8 +87,8 @@ module Jekyll::Spaceship
     # Examples:
     # ![dailymotion](https://www.dailymotion.com/video/x7tgcev)
     # ![dailymotion](https://dai.ly/x7tgcev?height=100%&width=400)
-    def handle_dailymotion(content)
-      handle_media(content, {
+    def handle_dailymotion(element)
+      handle_media(element, {
         media_type: 'iframe',
         host: '(https?:)?\\/\\/.*dai.?ly.*',
         id: '(?<=video\\/|\\/)([a-zA-Z0-9\\_\\-]+)',
@@ -92,8 +99,8 @@ module Jekyll::Spaceship
     # Examples:
     # ![spotify](//open.spotify.com/track/4Dg5moVCTqxAb7Wr8Dq2T5)
     # ![spotify](//open.spotify.com/track/37mEkAaqCE7FXMvnlVA8pp?width=400)
-    def handle_spotify(content)
-      handle_media(content, {
+    def handle_spotify(element)
+      handle_media(element, {
         media_type: 'iframe',
         host: '(https?:)?\\/\\/open\\.spotify\\.com\\/track\\/',
         id: '(?<=track\\/)([a-zA-Z0-9\\_\\-]+)',
@@ -104,8 +111,8 @@ module Jekyll::Spaceship
 
     # Examples:
     # ![soundcloud](//soundcloud.com/aviciiofficial/preview-avicii-vs-lenny)
-    def handle_soundcloud(content)
-      handle_media(content, {
+    def handle_soundcloud(element)
+      handle_media(element, {
         media_type: 'iframe',
         id_from: 'html',
         host: '(https?:)?\\/\\/soundcloud\\.com\\/.+\\/[^\\?]+',
@@ -116,71 +123,53 @@ module Jekyll::Spaceship
       })
     end
 
-    def handle_media(content, data)
+    def handle_media(element, data)
       host = data[:host]
-      return content if content.sub(/#{host}/, '').nil?
+      src = element.get_attribute('src')
+      title = element.get_attribute('title')
+      id = data[:id_from] === 'html' ? '()' : data[:id]
+      match_data = src.match(/#{host}#{id}\S*/)
+      return if match_data.nil?
 
       media_type = data[:media_type]
       base_url = data[:base_url]
-      id = data[:id_from] === 'html' ? '()' : data[:id]
-      url = "(#{host}#{id}\\S*)"
-      title = '("(.*)".*){0,1}'
-
-      # pre-handle reference-style links
-      regex = /(\[(.*)\]:\s*(#{url}\s*#{title}))/
-      content.scan regex do |match_data|
-        match = match_data[0]
-        ref_name = match_data[1]
-        ref_value = match_data[2]
-        content = content.gsub(match, '')
-          .gsub(/\!\[(.*)\]\s*\[#{ref_name}\]/,
-            "![\1](#{ref_value})")
+      id = data[:id_from] === 'html' \
+        ? get_id_from_html(src, data[:id]) \
+        : match_data[2]
+      qs = src.match(/(?<=\?)(\S*?)$/)
+      qs = Hash[URI.decode_www_form(qs.to_s)].reject do |k, v|
+        next true if v == id or v == ''
       end
 
-      # handle inline-style links
-      regex = /(\!\[(.*)\]\(.*#{url}\s*#{title}\))/
-      content.scan regex do |match_data|
-        url = match_data[2]
-        id = data[:id_from] === 'html' \
-          ? get_id_from_html(url, data[:id]) \
-          : match_data[4]
-        title = match_data[6]
-        qs = url.match(/(?<=\?)(\S*?)$/)
-        qs = Hash[URI.decode_www_form(qs.to_s)].reject do |k, v|
-          next true if v == id or v == ''
-        end
+      cfg = self.config['default'].clone
+      cfg['id'] = qs['id'] || cfg['id']
+      cfg['class'] = qs['class'] || cfg['class']
+      cfg['style'] = qs['style'] || cfg['style']
+      cfg['id'] = cfg['id'].gsub('{id}', id)
+      cfg['class'] = cfg['class'].gsub('{id}', id)
 
-        cfg = self.config['default'].clone
-        cfg['id'] = qs['id'] || cfg['id']
-        cfg['class'] = qs['class'] || cfg['class']
-        cfg['style'] = qs['style'] || cfg['style']
-        cfg['id'] = cfg['id'].gsub('{id}', id)
-        cfg['class'] = cfg['class'].gsub('{id}', id)
-
-        cfg['src'] = URI(base_url ? "#{base_url}#{id}" : url).tap do |v|
-          v.query = URI.encode_www_form(qs) if qs.size > 0
-        end
-
-        case media_type
-        when 'audio'
-          cfg['autoplay'] = qs['autoplay'] || data[:autoplay] || cfg['autoplay']
-          cfg['loop'] = qs['loop'] || data[:loop] || cfg['loop']
-          cfg['style'] += ';display: none;' if qs['hidden']
-          content = handle_audio(content, { target: match_data[0], cfg: cfg })
-        when 'iframe'
-          cfg['title'] = title
-          cfg['width'] = qs['width'] || data[:width] || cfg['width']
-          cfg['height'] = qs['height'] || data[:height] || cfg['height']
-          cfg['frameborder'] = qs['frameborder'] || cfg['frameborder']
-          cfg['allow'] ||= cfg['allow']
-          content = handle_iframe(content, { target: match_data[0], cfg: cfg })
-        end
-        self.handled = true
+      cfg['src'] = URI(base_url ? "#{base_url}#{id}" : src).tap do |v|
+        v.query = URI.encode_www_form(qs) if qs.size > 0
       end
-      content
+
+      case media_type
+      when 'audio'
+        cfg['autoplay'] = qs['autoplay'] || data[:autoplay] || cfg['autoplay']
+        cfg['loop'] = qs['loop'] || data[:loop] || cfg['loop']
+        cfg['style'] += ';display: none;' if qs['hidden']
+        handle_audio(element, { cfg: cfg })
+      when 'iframe'
+        cfg['title'] = title
+        cfg['width'] = qs['width'] || data[:width] || cfg['width']
+        cfg['height'] = qs['height'] || data[:height] || cfg['height']
+        cfg['frameborder'] = qs['frameborder'] || cfg['frameborder']
+        cfg['allow'] ||= cfg['allow']
+        handle_iframe(element, { cfg: cfg })
+      end
+      self.handled = true
     end
 
-    def handle_audio(content, data)
+    def handle_audio(element, data)
       cfg = data[:cfg]
       html = "<audio"\
         " id=\"#{cfg['id']}\""\
@@ -194,10 +183,11 @@ module Jekyll::Spaceship
         " Here is a <a href=\"#{cfg['src']}\">link to download the audio</a>"\
         "instead. </p>"\
         "</audio>"
-      content.gsub(data[:target], html)
+      doc = Nokogiri::XML(html)
+      element.replace(doc.children.first)
     end
 
-    def handle_iframe(content, data)
+    def handle_iframe(element, data)
       cfg = data[:cfg]
       html = "<iframe"\
         " id=\"#{cfg['id']}\""\
@@ -211,7 +201,8 @@ module Jekyll::Spaceship
         " frameborder=\"#{cfg['frameborder']}\""\
         " allowfullscreen>"\
         "</iframe>"
-      content.gsub(data[:target], html)
+      doc = Nokogiri::XML(html)
+      element.replace(doc.children.first)
     end
 
     def get_id_from_html(url, pattern)
